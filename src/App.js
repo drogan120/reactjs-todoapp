18b8fbfec660e16e378e33aa426d91bcd64782bd
drogan120 <alimahmudin.hp@gmail.com>
2020-09-07 21:47:32 +0700

    input handler

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ import TodoList from "./components/TodoList";
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
         <h4>DevEd Todo List</h4>
       </header>
-      <Form />
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
       <TodoList />
     </div>
   );
